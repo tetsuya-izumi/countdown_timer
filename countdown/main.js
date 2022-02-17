f097502a3c52ab2a4b009fdc4564acef25a67efb
ttyizm00 <38588229+tetsuya-izumi@users.noreply.github.com>
2022-02-18 07:39:09 +0900

    Add files via upload

--- countdown/main.js
+++ countdown/main.js
@@ -1,11 +1,22 @@
-btn_start.addEventListener('click', function () {
-    var countdown_time = document.getElementById('time')
-    
-    // 入力された値を十進数の数値として扱う
-    var calc_origin1 = parseInt(countdown_time.value, 10)
-    
-    console.log(calc_origin1);
+'use strict';
+function countdown(due) {
+    // 定数nowに現在時刻を代入
+    const now = new Date();
+    // 定数restに未来時刻-現在時刻の計算結果を代入
+    const rest = due.getTime() - now.getTime();
+    // 定数secに残り時間rest（ミリ秒）を秒単位にして代入
+    const sec = Math.floor(rest/1000);
+    return sec;
+}
 
-});
+let goal = new Date();
 
+console.log(goal);
 
+goal.setHours(23);
+goal.setMinutes(59);
+goal.setSeconds(59);
+
+console.log(countdown(goal));
+
+// 参考URL https://qiita.com/tatsuhiko-nakayama/items/effe8a1c1227514e3fcb
