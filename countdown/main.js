119a01c2cf3f824e55d8b2ca8d62bcb38103bb2a
ttyizm00 <38588229+tetsuya-izumi@users.noreply.github.com>
2022-02-18 07:58:11 +0900

    完成版です

--- countdown/main.js
+++ countdown/main.js
@@ -17,7 +17,27 @@ let goal = new Date();
 goal.setHours(23);
 goal.setMinutes(59);
 goal.setSeconds(59);
-
 console.log(countdown(goal));
+const counter = countdown(goal);
+const time = `${counter[0]}時間${counter[1]}分${counter[2]}`;
+console.log(time);
+document.getElementById('timer').textContent = time;
 
+function recalc() {
+    console.log(countdown(goal));
+    const counter = countdown(goal);
+    const time = `${counter[0]}時間${counter[1]}分${counter[2]}秒`
+    console.log(time);
+    document.getElementById('timer').textContent = time;
+    // ※次の一行を忘れずに追加！！
+    refresh();
+  }
+  
+  // ここから
+  function refresh() {
+    setTimeout(recalc, 1000);
+  }
+  
+  recalc();
+  // ここまでを追加
 // 参考URL https://qiita.com/tatsuhiko-nakayama/items/effe8a1c1227514e3fcb
